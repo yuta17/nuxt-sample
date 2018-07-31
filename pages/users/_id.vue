8fdf827dec1f125cd7d23c6c2053f6fa1cc5ae20
Hassan <usgitan@gmail.com>
2018-07-31 23:45:51 +0900

    add users page

---
+++ pages/users/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData ({ params }) {
+    const id = params;
+    return {
+      id
+    }
+  }
+}
+</script>
